900211c20dda2b35fb0835e0eddaa1836ce38dae
mit prajapatie <mitprajapati544@gmail.com>
2025-02-21 16:33:45 +0000

    environment_setUp

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>this the initial stage of the project....</h1>
+      <h4>loading....</h4>
+    </>
+  );
+}
