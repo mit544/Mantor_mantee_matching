98e78647bc31fb8197c28bc65a89d13f055c19ae
Mit J. Prajapati <mitprajapati544@gmail.com>
2025-02-21 16:50:33 +0000

    check

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>this the initial stage of the project....</h1>
-      <h4>loading....</h4>
+      <h4>loading...</h4>
     </>
   );
 }
