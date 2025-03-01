51160102caeda4857c35e31935760aef69165b8a
Mit J. Prajapati <mitprajapati544@gmail.com>
2025-03-01 14:51:05 +0000

    Nav_bar

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../src/components/navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>this the initial stage of the project....</h1>
+      <h4>loading...</h4>
+    </>
+  );
+}
